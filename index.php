4ef0196715c72f38c0f0fb8a192e508d6a9be72c
Fabio Rizzaro <fabio.rizzaro@gmail.com>
2023-11-08 16:42:06 -0300

    v0

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+ 
+echo 'Hello World!'.PHP_EOL;
+ 
+$servername = getenv('MYSQL_IP');
+$username = getenv('MYSQL_ROOT_USER');
+$password = getenv('MYSQL_ROOT_PASSWORD');
+ 
+
+$conn = mysqli_connect($servername, 'root', 'password');
+
+if (!$conn) {
+   exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+}
+ 
+echo 'Successful database connection!'.PHP_EOL;
